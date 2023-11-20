0e6682f738f6356afc0540ed36191e3c5fae396c
MateuszSudejko <mat.sud31@gmail.com>
2023-11-20 17:45:44 +0100

    amended admin panel and manage account pages

--- src/Components/AdminPanel/AdminPanel.tsx
+++ src/Components/AdminPanel/AdminPanel.tsx
@@ -1,25 +1,85 @@
-import React from 'react';
-import './AdminPanel.css'; // Import the CSS file
+import './AdminPanel.css';import React from 'react';
+import { AppBar, Tab, Tabs, Typography, Box, Paper, Button } from '@mui/material';
+
+// Sample data for users and games (replace this with your actual data)
+const users = [
+    { id: 1, username: 'user1', blocked: false },
+    { id: 2, username: 'user2', blocked: true },
+    // Add more users as needed
+];
+
+const games = [
+    { id: 1, name: 'Game 1' },
+    { id: 2, name: 'Game 2' },
+    // Add more games as needed
+];
 
 const AdminPanel = () => {
+    const [tabValue, setTabValue] = React.useState(0);
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabValue(newValue);
+    };
+
+    const handleBlockUser = (userId: number) => {
+        // Toggle the block status of the user (For demo purposes, update state directly)
+        const updatedUsers = users.map((user) =>
+            user.id === userId ? { ...user, blocked: !user.blocked } : user
+        );
+        // Replace this with your actual logic to update user block status
+        console.log(`User with ID ${userId} blocked/unblocked`);
+        console.log(updatedUsers); // Updated user list
+    };
+
+    const renderUsersTab = () => (
+        <Paper>
+            <Typography variant="h5">List of Users</Typography>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.username} - {user.blocked ? 'Blocked' : 'Active'}
+                        <Button onClick={() => handleBlockUser(user.id)}>
+                            {user.blocked ? 'Unblock' : 'Block'}
+                        </Button>
+                    </li>
+                ))}
+            </ul>
+        </Paper>
+    );
+
+    const renderGamesTab = () => (
+        <Paper>
+            <Typography variant="h5">List of Games</Typography>
+            <ul>
+                {games.map((game) => (
+                    <li key={game.id}>{game.name}</li>
+                ))}
+            </ul>
+        </Paper>
+    );
+
+    const renderTabContent = (index: number) => {
+        switch (index) {
+            case 0:
+                return renderUsersTab();
+            case 1:
+                return renderGamesTab();
+            default:
+                return null;
+        }
+    };
 
     return (
-        <div className="admin-panel">
-            <aside className="sidebar">
-                <ul className="sidebar-menu">
-                    <li className="menu-item active">Dashboard</li>
-                    <li className="menu-item">Users</li>
-                    <li className="menu-item">Settings</li>
-                </ul>
-            </aside>
-            <main className="main-content">
-                <h1>Dashboard</h1>
-                {/* Add different components for different sections */}
-                <div className="content-section">
-                    {/* Content for the Dashboard */}
-                    <p>Welcome to the Admin Panel Dashboard!</p>
-                </div>
-            </main>
+        <div className='container'>
+        <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            <AppBar position="static">
+                <Tabs value={tabValue} onChange={handleTabChange}>
+                    <Tab label="Users" />
+                    <Tab label="Games" />
+                </Tabs>
+            </AppBar>
+            {renderTabContent(tabValue)}
+        </Box>
         </div>
     );
 };
--- src/Components/UserManagement/UserManagement.tsx
+++ src/Components/UserManagement/UserManagement.tsx
@@ -1,54 +1,74 @@
-import React from 'react';
-import './UserManagement.css'; // Import the CSS file
+import React, { useState } from 'react';
+import { TextField, Button, Grid, Typography, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, Box } from '@mui/material';
+import './UserManagement.css'
 
 const UserManagement = () => {
-    const users = [
-        { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin' },
-        { id: 2, name: 'Jane Doe', email: 'jane@example.com', role: 'User' },
-        // Add more user data as needed
+    // State for password change field
+    const [newPassword, setNewPassword] = useState('');
+
+    // Sample data for played games (replace this with your actual data)
+    const playedGames = [
+        { id: 1, name: 'Game 1', score: 150 },
+        { id: 2, name: 'Game 2', score: 200 },
+        // Add more games as needed
     ];
 
+    // Handler for password change
+    const handlePasswordChange = () => {
+        // Implement password change logic here
+        console.log('New password:', newPassword);
+        // You may want to send this to an API or update state accordingly
+    };
+
     return (
-        <div className="user-management">
-            <aside className="sidebar">
-                <ul className="sidebar-menu">
-                    <li className="menu-item">Dashboard</li>
-                    <li className="menu-item active">Users</li>
-                    <li className="menu-item">Settings</li>
-                </ul>
-            </aside>
-            <main className="main-content">
-                <h1>User Management</h1>
-                <div className="user-list">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {users.map((user) => (
-                            <tr key={user.id}>
-                                <td>{user.id}</td>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.role}</td>
-                                <td>
-                                    <button>Edit</button>
-                                    <button>Delete</button>
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                </div>
-            </main>
+        <div className='container'>
+            <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={3}>
+            {/* Password Change Field */}
+            <Grid item xs={12}>
+                <Typography variant="h5">Change Password</Typography>
+                <TextField
+                    className="newpasswordfield"
+                    label="New Password"
+                    type="password"
+                    variant="outlined"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                />
+                <Button variant="contained" onClick={handlePasswordChange}>
+                    Change Password
+                </Button>
+            </Grid>
+
+            {/* List of Played Games */}
+            <Grid item xs={12}>
+                <Typography variant="h5">Played Games</Typography>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Game ID</TableCell>
+                                <TableCell>Game Name</TableCell>
+                                <TableCell>Score</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {playedGames.map((game) => (
+                                <TableRow key={game.id}>
+                                    <TableCell>{game.id}</TableCell>
+                                    <TableCell>{game.name}</TableCell>
+                                    <TableCell>{game.score}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
+            </Box>
         </div>
     );
 };
 
 export default UserManagement;
+
