3505369a4b223ced3759362957ce2253013c3cce
filip01 <filip.rak.20001@gmail.com>
2025-01-31 11:17:32 +0100

    fix global

--- src/Constants/global.js
+++ src/Constants/global.js
@@ -1,4 +1,4 @@
-const host_back = process.env.REACT_APP_BACKEND
-const host_front = process.env.REACT_APP_FRONTEND
+const host_back = 'http://104.197.206.183:3000'
+const host_front = 'http://34.133.171.61'
 
 export { host_back, host_front }
