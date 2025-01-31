c6d97597741879ce9b9f6e95dd2c952f8c7fde35
filip01 <filip.rak.20001@gmail.com>
2025-01-31 11:14:08 +0100

    add fial setup for now no env

--- src/Constants/global.js
+++ src/Constants/global.js
@@ -1,4 +1,4 @@
-const host_back = 'http://34.125.82.154'
-const host_front = 'http://34.125.86.65'
+const host_back = process.env.REACT_APP_BACKEND
+const host_front = process.env.REACT_APP_FRONTEND
 
 export { host_back, host_front }
